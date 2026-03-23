8a76b49baeedcf6824b6762af4068c44f3c89c4d
Bradley Hanson <brhanso@microsoft.com>
2026-03-23 05:42:59 -0700

    chore(examples): finalize barrel export for all 8 example configs

--- examples/configs/index.ts
+++ examples/configs/index.ts
@@ -1,6 +1,18 @@
-// Barrel export for all example form configs
-// JSON configs are imported directly; bootstrap files register side effects
+// JSON configs
+export { default as patientIntakeConfig } from "./patient-intake.json";
+export { default as jobApplicationConfig } from "./job-application.json";
+export { default as supportTicketConfig } from "./support-ticket.json";
+export { default as expenseReportConfig } from "./expense-report.json";
+export { default as eventRegistrationConfig } from "./event-registration.json";
+export { default as userSettingsConfig } from "./user-settings.json";
+export { default as surveyConfig } from "./survey.json";
+export { default as vehicleInsuranceQuoteConfig } from "./vehicle-insurance-quote.json";
 
-// JSON configs (to be added as each config is created)
-
-// Bootstrap functions (to be added as each config is created)
+// Bootstrap functions (side-effect registrations)
+export { bootstrapPatientIntake } from "./patient-intake.bootstrap";
+export { bootstrapJobApplication } from "./job-application.bootstrap";
+export { bootstrapExpenseReport } from "./expense-report.bootstrap";
+export { bootstrapEventRegistration } from "./event-registration.bootstrap";
+export { bootstrapUserSettings } from "./user-settings.bootstrap";
+export { bootstrapSurvey } from "./survey.bootstrap";
+export { bootstrapVehicleInsuranceQuote } from "./vehicle-insurance-quote.bootstrap";
